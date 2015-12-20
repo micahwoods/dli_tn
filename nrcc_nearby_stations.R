25668a78d2eb2c245284479f510650963355f3ca
micah <micah@asianturfgrass.com>
2015-12-20 18:02:27 +0700

    generates the charts I was trying to produce

--- nrcc_nearby_stations.R
+++ nrcc_nearby_stations.R
@@ -1,10 +1,8 @@
 # to estimate DLI for Memphis, Nashville, Chattanooga, & Knoxvile
 # first checking Rs for some nearby locations
 
-library(dplyr)
-library(lubridate)
-library(ggplot2)
-library(cowplot)
+source("r/libraries.R")
+source("r/functions.R")
 
 hol <- read.table("http://www1.ncdc.noaa.gov/pub/data/uscrn/products/daily01/2015/CRND0103-2015-MS_Holly_Springs_4_N.txt",
                   header = FALSE)
@@ -47,102 +45,37 @@ bow <- read.table("http://www1.ncdc.noaa.gov/pub/data/uscrn/products/daily01/201
 bow$loc <- "Bowling Green, KY"
 
 rawAll <- rbind.data.frame(hol, wat, bat, cro, cha, ash, bow, gad)
-  
-selectFunction <- function(x) {
-  selectedCols <- x[, c(2, 5, 6, 7, 8, 11, 29)]
-  colnames(selectedCols) <- c("date", "latitude", "maxTemp", "minTemp",
-                             "meanTemp", "Rs", "location")
-  return(selectedCols)
-}
-  
+
+# apply selectFunction to select columns of interest and name them  
 all <- selectFunction(rawAll)  
 
 all$date <- ymd(all$date)
 
+# remove the missing data
 all <- filter(all, Rs > -5000 &
                 minTemp > -5000 &
                 maxTemp > -5000 &
                 meanTemp > -5000)
 
+# convert from global irradiance to PAR as per Meek, 1984
 all$dli <- all$Rs * 2.04
 
-p <- ggplot(data = all, aes(x = date, y = dli))
-p + geom_point() +
-  facet_wrap(~ location) +
-  background_grid(major = "xy", minor = "xy")
 
-p <- ggplot(data = all, aes(x = dli))
-p + geom_histogram(fill = "white", colour = "black") +
-  facet_wrap(~ location, ncol = 2)
-
-hot <- filter(all, meanTemp >= 24)
-  
-p <- ggplot(data = hot, aes(x = dli))
-p + geom_histogram(fill = "white", colour = "black") +
-  scale_y_continuous(breaks = seq(0, 10, 2)) +
-  facet_wrap(~ location, ncol = 2)
-
-
-# calculate Ra based on input of latitude and day of year
-functionRa <- function(x, y) {
- 
-   # gives first constant
-  k.1 <- (24*60)/pi  
-  
-  # the solar constant
-  solar.constant <- 0.0820  
-  
-  day.of.year <- yday(y) # gives the day of the year as an integer
-  
-  # next lines give inverse relative distance Earth-Sun and solar declination
-  inverse.distance <- 1 + 0.033 * cos(((2 * pi) / 365) * day.of.year)
-  solar.declination <- 0.409 * sin((((2 * pi) / 365) * day.of.year) - 1.39)
-  
-  latitude.radians <- (pi / 180) * (x) # convert latitude to radians
-  sunset.hour.angle <- acos(-tan(latitude.radians) * tan(solar.declination))
-  
-  # the following equation now calculates the Ra, extraterrestrial irradiance
-  R.a <- ((k.1 * solar.constant) * inverse.distance) *
-    (sunset.hour.angle * sin(latitude.radians) * sin(solar.declination) +
-       cos(latitude.radians) * cos(solar.declination) * sin(sunset.hour.angle))
-  
-  return(R.a)
-}
-
+# calculate Ra for these locations
 all$Ra <- functionRa(all$latitude, all$date)
 
-# now to estimate DLI based on
-# the estimated Rs for a location based on Hargreave
-# where x = Ra, y = min temp, and z = max temp
-
-hargreave <- function(x, y, z) {
-  
-  R.s <- 0.16 * sqrt(z - y) * x
-  return(R.s)
-}
-
+# calculate Rs using Hargreaves' radiation formula
 all$RsCalc <- hargreave(all$Ra, all$minTemp, all$maxTemp)
 
-p <- ggplot(data = all, aes(x = RsCalc, y = Rs))
-p + geom_point(alpha = 0.4) +
-  geom_smooth(se = FALSE, method = 'lm') + 
-  geom_abline(intercept = 0, slope = 1, colour = "red") +
-  facet_wrap(~ location, ncol = 4) +
-  scale_x_continuous(limits = c(-1, 32)) +
-  scale_y_continuous(limits = c(-1, 32)) +
-  background_grid(major = "xy")
+# after checking data in various plots in the "r/plots.R" file
+# the Rs calculated from Hargreaves equation can be adjusted for these
+# regional data
 
 yo <- lm(Rs ~ RsCalc, data = all)
-
 summary(yo)
 
+# based on the regression above, correct the RsCalc
 all$RsCalcCorrected <- -2.71108 + all$RsCalc * 1.09444
 
-# what I am really interested in -- the distribution of DLI
-# is it roughly same in actual Rs, and in RsCalc, location by location?
-
-p <- ggplot(data = all, aes(x = Rs))
-p + geom_histogram(fill = "red", colour = "red", alpha = 0.5) +
-  geom_histogram(data = all, aes(x = RsCalc),
-                 fill = "blue", colour = "blue", alpha = 0.5) +
-  facet_wrap(~ location, ncol = 4)
+# based on corrected Rs, estimate a DLI
+all$dliCalc <- all$RsCalcCorrected * 2.04
---
+++ tn_four_cities.R
@@ -0,0 +1,144 @@
+# use Hargreaves' equation to estimate Rs (and then DLI)
+# for 2015 at Memphis, Nashville, Chattanooga, and Knoxville
+
+# read data file which I've filtered to airports at Memphis, Nashville,
+# Knoxville, and Chattanooga
+# data are from http://www.ncdc.noaa.gov/cdo-web/
+
+tn <- read.csv("data/tn_2015.csv",
+               header = TRUE)
+
+# assign a city name
+tn$city <- ifelse(tn$STATION == "GHCND:USW00013882", "Chattanooga",
+                  ifelse(tn$STATION == "GHCND:USW00013891", "Knoxville",
+                         ifelse(tn$STATION == "GHCND:USW00013893", "Memphis", "Nashville")))
+
+tn$date <- ymd(tn$DATE)
+
+# calculate Ra, the extraterrestrial radiation
+tn$Ra <- functionRa(tn$LATITUDE, tn$date)
+
+# adjust temperature decimal point
+tn$minTemp <- tn$TMIN / 10
+tn$maxTemp <- tn$TMAX / 10
+
+# calculate Rs and then DLI
+tn$RsCalc <- hargreave(tn$Ra, tn$minTemp, tn$maxTemp)
+tn$dliCalc <- tn$RsCalc * 2.04
+
+tn$date1 <- as.Date(tn$date)
+
+# plot by day of year
+p <- ggplot(data = tn, aes(x = date1, y = dliCalc))
+p + geom_point(alpha = 0.3) +
+  geom_smooth(enp.target = 15, se = FALSE) +
+  facet_wrap(~ city, ncol = 2) +
+  background_grid(major = "xy") +
+  geom_abline(intercept = 40, slope = 0, 
+              colour = "dark green", linetype = "dotted") +
+  geom_abline(intercept = 32.8, slope = 0, 
+              colour = "red", linetype = "dotted") +
+  scale_x_date(breaks = "1 month", labels=date_format("%b"),
+               expand = c(7/365, 4/365)) +
+  scale_y_continuous(limits = c(0, 60),
+                     breaks = seq(0, 60, 10)) +
+  labs(x = "",
+       y = bquote("Estimated daily light integral (DLI) in 2015, mol/"*m^2*"/day")) 
+
+# plot a histogram with 6 bins
+p <- ggplot(data = tn, aes(x = dliCalc))
+p + geom_histogram(fill = "white", colour = "black",
+                   binwidth = 10) +
+  scale_x_continuous(limits = c(0, 60), breaks = seq(0, 60, 10)) +
+  facet_wrap(~ city, ncol = 2) +
+  background_grid(major = "xy", minor = "y") +
+  labs(x = bquote("Estimated daily light integral (DLI) in 2015, mol/"*m^2*"/day"),
+       y = "Number of days")
+
+# plot a histogram with 6 bins when temp >= 20
+tn$meanTemp <- (tn$minTemp + tn$maxTemp) / 2
+tnHot <- filter(tn, meanTemp >= 20)
+
+p <- ggplot(data = tnHot, aes(x = dliCalc))
+p + geom_histogram(fill = "white", colour = "black",
+                   binwidth = 10) +
+  scale_x_continuous(limits = c(0, 60), breaks = seq(0, 60, 10)) +
+  facet_wrap(~ city, ncol = 2) +
+  background_grid(major = "xy", minor = "y") +
+  labs(x = bquote("Estimated DLI in 2015 when average daily temperature was at least 68°F, mol/"*m^2*"/day"),
+       y = "Number of days")
+
+dliCut40 <- filter(tn, dliCalc >= 40)
+dliCut32 <- filter(tn, dliCalc >= 32.8)
+
+summary40 <- dliCut40 %>%
+  group_by(city) %>%
+  summarise(length(dliCalc))
+
+summary32 <- dliCut32 %>%
+  group_by(city) %>%
+  summarise(length(dliCalc))
+
+tn$gpC4 <- c4gp(tn$meanTemp)
+tn$maxDLI <- tn$Ra * 0.75 * 2.04
+tn$dliIndex <- tn$dliCalc / tn$maxDLI
+tn$growthIndex <- tn$gpC4 * tn$dliIndex
+
+cha1 <- filter(tn, city == "Chattanooga")
+kno1 <- filter(tn, city == "Knoxville")
+mem1 <- filter(tn, city == "Memphis")
+nas1 <- filter(tn, city == "Nashville")
+
+# adjust for the fewer dates [missing data] of Knoxville
+dateVector <- kno1$date
+
+memAdjust <- subset(mem1, date %in% dateVector)
+chaAdjust <- subset(cha1, date %in% dateVector)
+nasAdjust <- subset(nas1, date %in% dateVector)
+
+
+cha <- filter(tnHot, city == "Chattanooga")
+kno <- filter(tnHot, city == "Knoxville")
+mem <- filter(tnHot, city == "Memphis")
+nas <- filter(tnHot, city == "Nashville")
+
+# chart of summed DLI when temperature is >= 20
+p <- ggplot(data = cha, aes(x = date, y = cumsum(dliCalc)))
+p + geom_line(aes(colour = city)) +
+  geom_line(data = kno, aes(x = date, y = cumsum(dliCalc),
+                            colour = city)) +
+  geom_line(data = mem, aes(x = date, y = cumsum(dliCalc),
+                            colour = city)) +
+  geom_line(data = nas, aes(x = date, y = cumsum(dliCalc),
+                            colour = city)) +
+  background_grid(major = "xy", minor = "xy") +
+  scale_x_datetime(limits = c(ymd(20150315, ymd(20151215))),
+                   breaks = date_breaks("2 months")) +
+  labs(x = "",
+        y = "Cumulative sum of estimated DLI in 2015", 
+       title = "Days with average daily temperature of 68°F or above") +
+  scale_color_brewer(palette = "Set1")
+  
+
+# chart of growth index cumulative through the year, GP * dliCalc/maxDLI
+
+# chart of summed growth index
+p <- ggplot(data = chaAdjust, aes(x = date, y = cumsum(growthIndex)))
+p + geom_line(aes(colour = city)) +
+  geom_line(data = kno1, aes(x = date, y = cumsum(growthIndex),
+                            colour = city)) +
+  geom_line(data = memAdjust, aes(x = date, y = cumsum(growthIndex),
+                            colour = city)) +
+  geom_line(data = nasAdjust, aes(x = date, y = cumsum(growthIndex),
+                            colour = city)) +
+  background_grid(major = "xy", minor = "xy") +
+  #scale_x_datetime(limits = c(ymd(20150315, ymd(20151215))),
+   #                breaks = date_breaks("2 months")) +
+  labs(x = "",
+       y = "Cumulative sum of growth index") +
+  scale_colour_brewer(palette = "Set1") +
+  scale_y_continuous(limits = c(0, 100),
+                     breaks = seq(0, 100, 20)) +
+  annotate("text", x = ymd(20150415), y = 90, label = "For more about growth index see\nwww.blog.asianturfgrass.com/2015/09/a-dli-index.html")
+
+  
